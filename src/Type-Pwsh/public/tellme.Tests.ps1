1698a793ebdd6f7c02829718ba29e1d6e19902c0
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-05 17:50:33 +0000

    Fix (tests): Make tellme test more intuitive

--- src/Type-Pwsh/public/tellme.Tests.ps1
+++ src/Type-Pwsh/public/tellme.Tests.ps1
@@ -5,18 +5,17 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 Describe "tellme" -Tag 'unit' {
 
     It 'Given something, does that thing' {
-        $something = 'Write-Output "hello world"'
-        $thing = 'hello world'
+        $something = '1'
+        $thing = '1'
 
         tellme $something | Should -Be $thing
     }
 
     It 'Given something(s), does those things' {
-        $something1 = 'Write-Output'
-        $something2 = 'hello  world'
-        $things = 'hello', 'world'
+        $something1 = 'echo'
+        $something2 = '2'
 
-        tellme $something1 $something2 | Should -Be $things
+        tellme $something1 $something2 | Should -Be $something2
     }
 
 }
